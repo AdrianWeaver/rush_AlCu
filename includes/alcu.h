b824a93130925c462eca3e74026eff95b201bd74
Adrian WEAVER <aweaver@student.42.fr>
2022-02-12 20:16:34 +0100

    fixing prototype type for ft_game_loop, adding protection

--- includes/alcu.h
+++ includes/alcu.h
@@ -6,20 +6,20 @@
 /*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 21:37:23 by bregneau          #+#    #+#             */
-/*   Updated: 2022/02/12 18:44:52 by bregneau         ###   ########.fr       */
+/*   Updated: 2022/02/12 20:16:01 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ALCU_H
 # define ALCU_H
 
-#include "libft.h"
-#include "unistd.h"
-#include "alcu.h"
-#include "libftprintf.h"
-#include "get_next_line.h"
-#include "stdlib.h"
-#include "fcntl.h"
+# include "libft.h"
+# include "unistd.h"
+# include "alcu.h"
+# include "libftprintf.h"
+# include "get_next_line.h"
+# include "stdlib.h"
+# include "fcntl.h"
 
 # ifndef S_MAP
 #  define S_MAP
@@ -36,7 +36,7 @@ typedef struct s_map
 # ifndef ALCU_PARSING
 #  define ALCU_PARSING
 
-int	alcu_parsing(int fd, t_map *map);
+int		alcu_parsing(int fd, t_map *map);
 
 # endif
 
@@ -44,9 +44,13 @@ int	alcu_parsing(int fd, t_map *map);
 #  define FT_DISPLAY_BOARD
 
 void	ft_display_board(t_map *map);
-void	ft_game_loop(t_map *map);
-
 
 # endif
 
+# ifndef FT_GAME_LOOP
+#  define FT_GAME_LOOP
+
+void	ft_game_loop(t_map *map, int fake_stdin);
+
+# endif
 #endif
