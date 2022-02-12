0227e228f10ced9bd79a787d8f161214ac192af6
Adrian WEAVER <aweaver@student.42.fr>
2022-02-12 21:59:32 +0100

    removed ft_printf to comply with subject

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: bregneau <bregneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 18:39:03 by aweaver           #+#    #+#             */
-/*   Updated: 2022/02/12 21:44:58 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/02/12 21:58:39 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ int	main(int argc, char **argv)
 	{
 		if (alcu_parsing(0, &map) == -1)
 		{
-			ft_printf("ERROR\n");
+			ft_putstr("ERROR\n");
 			return (-1);
 		}
 	}
 	if (argc > 2)
 	{
-		ft_printf("ERROR\n");
+		ft_putstr("ERROR\n");
 		return (-1);
 	}
 	if (argc != 1)
@@ -40,12 +40,12 @@ int	main(int argc, char **argv)
 		fd = open(argv[1], O_RDONLY);
 		if (fd == -1)
 		{
-			ft_printf("ERROR\n");
+			ft_putstr("ERROR\n");
 			return (-1);
 		}
 		if (alcu_parsing(fd, &map) == -1)
 		{
-			ft_printf("ERROR\n");
+			ft_putstr("ERROR\n");
 			return (-1);
 		}
 	}
